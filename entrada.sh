aa3888531097f641f7a3b9a7dc067d1528ed7d66
Francisco <frruizal@unirioja.es>
2021-05-14 13:05:52 +0200

    carousel

--- entrada.sh
+++ entrada.sh
@@ -3,5 +3,3 @@
 #gatsby serve
 #gatsby develop -H 0.0.0.0
 node middleware.js
-
-#{post.node.relationships.field_image ? <Card.Img className="card-img-top h-20" src={myurl + post.node.relationships.field_image.uri.url} /> : ''}
--- src/components/lista-nodos.jsx
+++ src/components/lista-nodos.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { graphql, StaticQuery, Link} from 'gatsby';
-import {Button,Card,CardImg} from "react-bootstrap";
+import {Carousel} from "react-bootstrap";
 //Aqui se va a mostrar la lista de todos los nodos disponibles
 const Contenido=({data}) => (
   <StaticQuery
@@ -17,6 +17,13 @@ const Contenido=({data}) => (
                     title
                     path {
                         alias
+                    }
+                     relationships {
+                      field_imagen {
+                        uri{
+                          url
+                        }
+                      }
                     }
                     body {
                         value
@@ -52,40 +59,50 @@ const Contenido=({data}) => (
       const myurl =data.sitePlugin.pluginOptions.baseUrl;
 
       const articuloList= data.allNodeArticle.edges.map(post =>(
-
-        <Card bg="light">
         <div className="container">
+          <hr></hr>
           <div className="row">
-            <div className="col-12 col col-md-6 text center">
+            <h2 >{post.node.title}</h2>
+            <div className="col-12 col col-md-6 text left">
               {post.node.relationships.field_image ? <img src={myurl + post.node.relationships.field_image.uri.url} alt={post.node.relationships.field_image.alt} height={post.node.relationships.field_image.width}/> : <img src="https://www.agiliacenter.com/wp-content/uploads/2017/02/drupal-cms.png" />}
             </div>
-            <div className="col-12 col-md-6 text-center text-md-left align-self-md-center">
-              <h2 >{post.node.title}</h2>
-              <div dangerouslySetInnerHTML={{ __html: post.node.body.summary}}></div>
+            <div className="col-12 col-md-6 text-left">
+              <div dangerouslySetInnerHTML={{ __html: post.node.body.summary}}></div><br></br>
               <div align="right"> <Link to={post.node.path.alias} className="btn btn-outline-primary">Read more</Link></div><br></br>
             </div>
           </div>
         </div>
-        </Card>
-
 
 
         )
       );
 
-      const noticialist= data.allNodeNoticia.edges.map(notice =>(
-          <div>
-            <h2 > {notice.node.title}</h2>
-            <div align="right"><Link to={notice.node.path.alias} >Read more</Link></div>
-            <div dangerouslySetInnerHTML={{ __html: notice.node.body.summary }}></div>
+        const noticialist=
+        <div>
+          <br></br>
+          <h2 className="text-center">Tecnologías</h2>
+          <Carousel>
+            {data.allNodeNoticia.edges.map(notice =>(
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={myurl + notice.node.relationships.field_imagen.uri.url}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h2>{notice.node.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: notice.node.body.summary}}></div>
+            </Carousel.Caption>
+          </Carousel.Item>
+            )
+            )}
+        </Carousel>
+        </div>
 
-            <hr></hr>
-          </div>
 
-        )
-      );
 
-      return <section >{articuloList}{noticialist}</section>;
+
+      return <section >{noticialist}{articuloList}</section>;
     }}
   />)
 
